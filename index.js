1da5e7ee36d3a5ce96088433774774ccebaa73ec
Bora Manga <bora.mng525@gmail.com>
2019-10-23 15:47:26 +0200

    Simple Node Example

---
+++ index.js
@@ -0,0 +1,19 @@
+var rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y
+};
+function solveRect(l, b) {
+  console.log("solving for rect l= " + l + "and b= " + b);
+  if (l <= 0 || b <= 0) {
+    console.log(
+      "Rectangle dimensions should be greater than zero. l= " + l + "b= " + b
+    );
+  } else {
+    console.log("the area of rect is" + rect.area(l, b));
+    console.log("the perimeter of rect is" + rect.perimeter(l, b));
+  }
+}
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
